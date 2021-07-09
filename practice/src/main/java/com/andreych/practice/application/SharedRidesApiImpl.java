2820bd6be7d0ff0d314ec5718a1e798ac0cbad10
AndreyChar <87012789+AndreyChar@users.noreply.github.com>
2021-07-09 18:26:34 +0400

    Practice 6

--- practice/src/main/java/com/andreych/practice/application/SharedRidesApiImpl.java
+++ practice/src/main/java/com/andreych/practice/application/SharedRidesApiImpl.java
@@ -22,8 +22,24 @@ public class SharedRidesApiImpl implements SharedRidesApi {
         Set<Ride> srd = sharedRidesDao.getAllRides();
         for(Ride r : srd)
         {
+            Ride r1 = r;
             List<Order> orders = r.getOrderList();
-            
+            for(int i = 0; i < orders.size(); i++)
+            {
+                if(orders.get(i).getStartDateTime() == order.getStartDateTime())
+                {
+                    setOrderToRide(order, r);
+                }
+                else if(orders.get(i).getDrop().getAddress() == order.getDrop().getAddress())
+                {
+                    setOrderToRide(order,r);
+                }
+                else if(orders.get(i).getPickup().getAddress() == order.getPickup().getAddress())
+                {
+                    setOrderToRide(order,r);
+                }
+            }
+            if(!r.equals(r1)) rides.add(r);
         }
         return rides;
         //todo implement
